1e1a5c6f5319cc5c4dd5f880a1d7e4fe5f93168a
Matt Terenzio <mterenzio@gmail.com>
2015-10-16 12:47:15 -0400

    Look for a template in the theme first or use the default plugin version

--- wp-instant-articles.php
+++ wp-instant-articles.php
@@ -13,12 +13,17 @@ License URI: https://www.gnu.org/licenses/gpl-2.0.html
 
 add_action('init', 'instantArticles');
 function instantArticles(){
-        add_feed('instant-articles', 'getInstantArticlesTemplate');
+	add_feed('instant-articles', 'getInstantArticlesTemplate');
 }
 
 
 function getInstantArticlesTemplate(){
-	load_template( get_stylesheet_directory() . '/instant-articles-feed.php' );
+    if ( !file_exists(get_stylesheet_directory() . '/instant-articles-feed.php')) {
+      load_template( __DIR__ . '/instant-articles-template.php');
+    } else {
+      load_template( get_stylesheet_directory() . '/instant-articles-feed.php' );
+    }
 }
+
 
 ?>
